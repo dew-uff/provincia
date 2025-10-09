f3a984c79b4ddfcea96968b591a0ca3037fd64bc
Larissa Soares Reis <larissasoaresreis@gmail.com>
2025-10-08 21:37:48 -0300

    chore: Added TODO list
    Signed-off-by: Larissa Soares Reis <larissasoaresreis@gmail.com>

--- src/assets/presentation/pages/Login.tsx
+++ src/assets/presentation/pages/Login.tsx
@@ -16,6 +16,12 @@ export function LoginPage() {
             <div className='bg-white p-12 rounded-lg shadow-[0_20px_60px_rgba(0,0,0,0.3)] w-105 flex flex-col items-center'>
                 <h2 className='text-[#2563EB] mb-2 text-3xl font-bold'>ProvInCia</h2>
                 <p className='text-[#6B7280] text-[0.875rem] mb-9'>Provenance in Smart Cities</p>
+                /** TODO:
+                    - Componentizar formulário
+                        - Criar componentes para cada campo de entrada
+                        - Criar componente para o botão
+                    - Adicionar validação de campos                
+                */
                 <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="email">Email:</label>
